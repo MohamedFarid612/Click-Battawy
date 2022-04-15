8e0ea0dace6215eb75f8c13c6eead10416ffb441
Yousef Kotp <yousefkotp@outlook.com>
2022-04-15 17:32:49 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".photo").addEventListener("click",function()
-{
- var audio=new Audio("duck.mp3");
- audio.play();
-$(".photo").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-})
