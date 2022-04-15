0f8887afa786e2f4c067e15904aa7b02fe2a7ca0
Yousef Kotp <yousefkotp@outlook.com>
2022-04-15 17:30:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.querySelector(".photo").addEventListener("click",function()
 {
  var audio=new Audio("duck.mp3");
  audio.play();
-
+$(".photo").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 })
